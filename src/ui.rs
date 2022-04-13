c658d0498d00dfa1841443a59aef675249576c01
Adnan SOZUAN <adnan.sozuan@gmail.com>
2022-04-13 15:53:17 +0200

    Change layout of system information area:   Add subdivided areas.   Divide labels and values areas.

--- src/ui.rs
+++ src/ui.rs
@@ -6,10 +6,11 @@ use crossterm::{event, execute};
 use std::io;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::{mpsc, Arc};
+use std::time::Duration;
 use tui::backend::{Backend, CrosstermBackend};
 use tui::layout::{Alignment, Constraint, Direction, Layout, Rect};
 use tui::style::{Color, Modifier, Style};
-use tui::text::{Span, Text};
+use tui::text::{Span, Spans, Text};
 use tui::widgets::{Block, Borders, Gauge, Paragraph};
 use tui::{Frame, Terminal};
 
@@ -31,7 +32,7 @@ pub fn run_ui(
     loop {
         let mut incoming_info: SystemInfoDynamicData = SystemInfoDynamicData {
             cpu_usage: 0.0,
-            memory_usage_percentage: 0,
+            memory_usage_percentage: 0.0,
             page_memory_usage_percentage: 0,
             total_tasks_count: 0,
             running_tasks_count: 0,
@@ -82,6 +83,8 @@ fn ui<B: Backend>(f: &mut Frame<B>, system_info_static: SystemInfoStaticData,
         )
         .split(size);
 
+    // system info area is divided in two ares
+    // one for dynamic data on left, and one for static data on right
     let sys_info_areas = Layout::default()
         .direction(Direction::Horizontal)
         .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
@@ -96,60 +99,102 @@ fn ui<B: Backend>(f: &mut Frame<B>, system_info_static: SystemInfoStaticData,
         .style(Style::default());
 
     draw_title(f, system_info_static.computer_name.clone(), main_areas[0]);
-    draw_sys_info_dynamic(f, sys_info_areas[0], system_info_dynamic.cpu_usage,
-                          system_info_dynamic.memory_usage_percentage);
-    draw_sys_info_static(f, system_info_static, sys_info_areas[1]);
+    draw_sys_info_left(f, sys_info_areas[0], system_info_dynamic.cpu_usage,
+                       system_info_dynamic.memory_usage_percentage);
+    draw_sys_info_right(f, system_info_static, system_info_dynamic.total_tasks_count, system_info_dynamic.uptime, sys_info_areas[1]);
     // f.render_widget(process_block, main_areas[2]);
     draw_logo_block(f, main_areas[2]);
 }
 
 fn draw_title<B: Backend>(f: &mut Frame<'_, B>, hostname: String, area: Rect) {
+    let title = format!("riptop on {}", hostname);
+    let text = vec![
+        Span::styled(title, Style::default().fg(Color::Yellow)),
+    ];
     let title_block = Block::default()
-        .title(vec![
-            Span::styled("riptop on ", Style::default().fg(Color::Yellow)),
-            Span::from(hostname),
-        ])
+        .title(text.clone())
         .title_alignment(Alignment::Center)
         .style(Style::default().bg(Color::Blue));
     f.render_widget(title_block, area);
 }
 
-fn draw_sys_info_dynamic<B: Backend>(f: &mut Frame<'_, B>, area: Rect, cpu: f64, mem: u64) {
-    let sys_dyn_block = Block::default()
-        .title(vec![
-            Span::styled("CPU: ", Style::default().fg(Color::Yellow)),
-            Span::from("& Cie"),
-        ])
-        .borders(Borders::RIGHT)
-        .style(Style::default()); //.bg(Color::Green));
+fn draw_sys_info_left<B: Backend>(f: &mut Frame<'_, B>, area: Rect, cpu: f64, mem: f64) {
+    let columns = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([Constraint::Percentage(25), Constraint::Percentage(75)].as_ref())
+        .split(area);
+    let gauges_lines = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([Constraint::Percentage(33), Constraint::Percentage(33), Constraint::Percentage(33)].as_ref())
+        .split(columns[1]);
+    let labels = vec![
+        Spans::from(Span::styled("CPU: ", Style::default().fg(Color::Yellow))),
+        Spans::from(Span::styled("MEM: ", Style::default().fg(Color::Yellow))),
+        Spans::from(Span::styled("PGE: ", Style::default().fg(Color::Yellow))),
+    ];
+
+    let sys_static_labels_paragraph = Paragraph::new(labels.clone())
+        .alignment(Alignment::Left);
 
     let cpu_label = format!("{:.2}%", cpu);
-    let cpu_gauge = Gauge::default()
-        .block(sys_dyn_block)
-        .gauge_style(Style::default().fg(Color::Magenta).bg(Color::Black))
-        .label(cpu_label)
-        .ratio(0.8);
     let mem_label = format!("{}%", mem);
+    let cpu_gauge = Gauge::default()
+        .block(Block::default())
+        .gauge_style(Style::default().fg(Color::Magenta).bg(Color::Black))
+        .label(cpu_label.clone())
+        .percent(32);
+    let mem_gauge = Gauge::default()
+        .block(Block::default())
+        .gauge_style(Style::default().fg(Color::Magenta).bg(Color::Black))
+        .label(mem_label.clone())
+        .ratio(mem.clone() as f64);
+    let pge_gauge = Gauge::default()
+        .block(Block::default())
+        .gauge_style(Style::default().fg(Color::Magenta).bg(Color::Black))
+        .label(cpu_label.clone())
+        .percent(11);
     // let mem_gauge = Gauge::default()
     //     .block(sys_dyn_block)
     //     .gauge_style(Style::default().fg(Color::Magenta).bg(Color::Black))
     //     .label(mem_label)
     //     .ratio(0.8);
 
-    f.render_widget(cpu_gauge, area);
+    f.render_widget(sys_static_labels_paragraph, columns[0]);
+    f.render_widget(cpu_gauge, gauges_lines[0]);
+    f.render_widget(mem_gauge, gauges_lines[1]);
+    f.render_widget(pge_gauge, gauges_lines[2]);
 }
 
-fn draw_sys_info_static<B: Backend>(f: &mut Frame<'_, B>, system_info_static: SystemInfoStaticData, area: Rect) {
-    let sys_static_block = Block::default()
-        .title(vec![
-            Span::styled("Proc: ", Style::default().fg(Color::Yellow)),
-            Span::from(system_info_static.processor_name),
-            Span::styled("\n", Style::default().fg(Color::Yellow)),
-            Span::from(system_info_static.cpu_count.to_string()),
-        ])
-        .style(Style::default());
+fn draw_sys_info_right<B: Backend>(f: &mut Frame<'_, B>, system_info_static: SystemInfoStaticData,
+                                   task_count: usize, uptime: u64, area: Rect) {
+    let columns = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([Constraint::Percentage(25), Constraint::Percentage(75)].as_ref())
+        .split(area);
 
-    f.render_widget(sys_static_block, area);
+    let labels = vec![
+        Spans::from(Span::styled("Tasks: ", Style::default().fg(Color::Yellow))),
+        Spans::from(Span::styled("Size: ", Style::default().fg(Color::Yellow))),
+        Spans::from(Span::styled("Uptime: ", Style::default().fg(Color::Yellow))),
+        Spans::from(Span::styled("Proc: ", Style::default().fg(Color::Yellow))),
+    ];
+
+    let values = vec![
+        //Span::from("Proc: ", Style::default().fg(Color::Yellow)),
+        Spans::from(task_count.to_string()),
+        Spans::from(system_info_static.total_memory.to_string()),
+        Spans::from(uptime.to_string()),
+        Spans::from(system_info_static.processor_name),
+    ];
+
+
+    let sys_static_paragraph = Paragraph::new(labels.clone())
+        .alignment(Alignment::Left);
+    let sys_static_values_paragraph = Paragraph::new(values.clone())
+        .alignment(Alignment::Left);
+
+    f.render_widget(sys_static_paragraph, columns[0]);
+    f.render_widget(sys_static_values_paragraph, columns[1]);
 }
 
 pub const BANNER: &str = r#"
